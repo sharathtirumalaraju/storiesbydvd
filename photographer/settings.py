71f1f8f073c1d3351dbf8d5196a2054cb4c54301
sharathtirumalaraju <48446664+sharathtirumalaraju@users.noreply.github.com>
2024-09-11 11:59:15 -0500

    Update settings.py

--- photographer/settings.py
+++ photographer/settings.py
@@ -15,7 +15,13 @@ ALLOWED_HOSTS = ['storiesbydvd.azurewebsites.net']
 
 INSTALLED_APPS = [
     'portfolio',
-    'storages'
+    'storages',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
