44e334a31f8d57f6420cdea8ab1cacb2dfc8f1ce
sharathtirumalaraju <48446664+sharathtirumalaraju@users.noreply.github.com>
2024-09-12 11:26:02 -0500

    Update settings.py

--- photographer/settings.py
+++ photographer/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['storiesbydvd-hwc7ewhea2czf0fy.mexicocentral-01.azurewebsites.net']
+ALLOWED_HOSTS = ['storiesbydvd.azurewebsites.net']
 
 
 # Application definition
